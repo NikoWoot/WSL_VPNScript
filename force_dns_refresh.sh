9ef44305025cfb8bb5c3aeb5e49da02d3165b39a
Florin Idita <florin.idita@gmail.com>
2022-01-03 13:00:50 +0200

    powershell path may be different
    in my case C: is not in /mnt/c and wslpath convert the windows path to wsl

--- force_dns_refresh.sh
+++ force_dns_refresh.sh
@@ -13,7 +13,7 @@ force_dns_refresh() {
     echo "------------------------------------------"
 
     tmp_resolveconf=`mktemp`
-    powershell="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+    powershell=$(wslpath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe")
 
     dnssrvlist=`$powershell -Command '$x = Get-NetAdapter | Group-Object -AsHashtable -Property ifIndex; Get-DnsClientServerAddress -AddressFamily ipv4 | where {$x[$_.InterfaceIndex].Status -eq "Up"} | Select-Object -ExpandProperty ServerAddresses' | sed 's/\r//g'`
     {
